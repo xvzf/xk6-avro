a63e426a7280923c72e576b9cc550ea1efa25239
Matthias Riegler <matthias.riegler@traefik.io>
2021-12-08 16:11:31 +0100

    fix: use []byte for the generated avro; add errors to output for easier debugging
    Signed-off-by: Matthias Riegler <matthias.riegler@traefik.io>

--- avro.go
+++ avro.go
@@ -5,6 +5,8 @@ import (
 	"github.com/linkedin/goavro/v2"
 	"go.k6.io/k6/js/common"
 	"go.k6.io/k6/js/modules"
+
+  "fmt"
 )
 
 func init() {
@@ -28,27 +30,27 @@ func (*Avro) XNewCodec(ctxPtr *context.Context, schema string) interface{} {
 }
 
 // BinaryFromTextual converts an avro json encoded document into its binary representation.
-func (ac *AvroCodec) BinaryFromTextual(textual string) string {
+func (ac *AvroCodec) BinaryFromTextual(textual string) []byte {
 	native, _, err := ac.codec.NativeFromTextual([]byte(textual))
 	if err != nil {
-		return ""
+		return []byte(fmt.Sprintf("%v", err))
 	}
 	binary, err := ac.codec.BinaryFromNative(nil, native)
 	if err != nil {
-		return ""
+		return []byte(fmt.Sprintf("%v", err))
 	}
-	return string(binary)
+	return binary
 }
 
 // TextualFromBinary converts an avro binary encoded document into its textual representation.
 func (ac *AvroCodec) TextualFromBinary(binary []byte) string {
 	native, _, err := ac.codec.NativeFromBinary(binary)
 	if err != nil {
-		return ""
+		return fmt.Sprintf("%v", err)
 	}
 	textual, err := ac.codec.TextualFromNative(nil, native)
 	if err != nil {
-		return ""
+		return fmt.Sprintf("%v", err)
 	}
 	return string(textual)
 }
